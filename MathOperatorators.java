5fcc3b23abdf8c9290fd0cb9962a987194acea8e
DEVAT <atharvfordevelopment@gmail.com>
2025-05-12 18:27:14 +0530

    Math operations via operators

---
+++ MathOperatorators.java
@@ -0,0 +1,16 @@
+public class MathOperatorators {
+    
+    public static void main(String[] args) {
+        int money =  100100 ;
+        money = money + 1000;
+        System.out.println(money);
+        money = money - 1000;
+        System.out.println(money);
+        money = money * 8 ;
+        System.out.println(money);
+        money = money / 2;
+        System.out.println(money);
+        money = money % 2;
+        System.out.println(money);
+    }
+}
