4cd7ecdd5bfa4e91af0517a47043e9573cec2c6f
DEVAT <atharvfordevelopment@gmail.com>
2025-05-12 15:59:16 +0530

    Added suppress warings for unused variable for remving warnings

--- DataTypes.java
+++ DataTypes.java
@@ -1,9 +1,13 @@
 public class DataTypes{
     public static void main (String [] args) {
+    @SuppressWarnings("unused")
     int a = 10 ;
+    @SuppressWarnings("unused")
     double b = 1.0;
     boolean c = true ; 
+    @SuppressWarnings("unused")
     char symbol = '#' ;
+    @SuppressWarnings("unused")
     String name = "Hi" ;
     System.out.print(" It worked " + c);
 
