9681dc6c21165e47e86eb1a389c14bc81127f1f1
DEVAT <atharvfordevelopment@gmail.com>
2025-05-12 14:55:56 +0530

    Data-Types in java

---
+++ DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes{
+    public static void main (String [] args) {
+    int a = 10 ;
+    double b = 1.0;
+    boolean c = true ; 
+    char symbol = '#' ;
+    String name = "Hi" ;
+    System.out.print(" It worked " + c);
+
+    }
+}
