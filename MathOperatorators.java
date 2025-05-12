f6e1ea05607b4dac4bf586b0a87eacc54e98dadd
DEVAT <atharvfordevelopment@gmail.com>
2025-05-12 18:55:59 +0530

    input addded for calcuation

--- MathOperatorators.java
+++ MathOperatorators.java
@@ -1,16 +1,27 @@
+import java.util.Scanner;
+
 public class MathOperatorators {
-    
+
     public static void main(String[] args) {
-        int money =  100100 ;
+        int money = 100100;
         money = money + 1000;
         System.out.println(money);
         money = money - 1000;
         System.out.println(money);
-        money = money * 8 ;
+        money = money * 8;
         System.out.println(money);
         money = money / 2;
         System.out.println(money);
         money = money % 2;
         System.out.println(money);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the number");
+        Double cal = scanner.nextDouble();
+        cal = cal / 3;
+        System.out.println(cal);
+        cal = cal % 3;
+        System.out.println(cal);
+        scanner.close();
     }
 }
