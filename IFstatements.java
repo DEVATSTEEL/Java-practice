b0a7a619b02859904917fb9ca403809a29e8957a
DEVAT <atharvfordevelopment@gmail.com>
2025-05-13 15:51:08 +0530

    If statements in java

---
+++ IFstatements.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class IFstatements {
+
+    @SuppressWarnings("ConvertToTryWithResources")
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int age = reader.nextInt();
+
+        if (age >= 75) {
+            System.out.println("Ok Boomer");
+        } else if (age >= 18) {
+            System.out.println("you are an adult");
+        } else if (age <= 14) {
+            System.out.println("you are an child");
+
+        } else {
+            System.out.println("you are not an adult");
+        }
+        reader.close();
+    }
+
+}
