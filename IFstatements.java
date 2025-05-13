74b5ede7c3d927c13f74d4240194c78cb3afd95d
DEVAT <atharvfordevelopment@gmail.com>
2025-05-13 16:25:36 +0530

    added space

--- IFstatements.java
+++ IFstatements.java
@@ -6,7 +6,9 @@ public class IFstatements {
     @SuppressWarnings("ConvertToTryWithResources")
 
     public static void main(String[] args) {
+        
         Scanner reader = new Scanner(System.in);
+
         System.out.println("Enter the number ");
         int age = reader.nextInt();
 
