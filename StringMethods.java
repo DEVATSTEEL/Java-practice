0a744df856adfdb571d7a8b0aa5a6f601245fe49
DEVAT <atharvfordevelopment@gmail.com>
2025-05-14 18:39:41 +0530

    String methods in java

---
+++ StringMethods.java
@@ -0,0 +1,27 @@
+public class StringMethods{
+
+    public static void main(String[] args) {
+        
+    String name = " DEV ";
+
+    int result = name.length();
+
+   // boolean result = name.equalsIgnoreCase("DEV");
+   
+   // char result = name.charAt(0);
+
+   // boolean result = name.isEmpty();
+
+   // String result = name.toLowerCase();
+
+    // String result = name.toUpperCase();
+
+   // String result = name.trim(); // Kills empty space
+
+  // String result = name.replace('V' , 'I');
+
+   System.out.println(result);
+
+}
+
+}
