f3e78f7b23d42416799f288b5c3e5f3e78d973aa
DEVAT <atharvfordevelopment@gmail.com>
2025-05-14 19:20:59 +0530

    Wrapper classes in java

---
+++ WrapperClass.java
@@ -0,0 +1,74 @@
+// wrapper class = provides a way to use primitive data types as reference data types
+// reference data types contain useful methods
+// can be used with collections (ex.ArrayList)
+
+// primitive  // wrapper
+//  boolean    Boolean
+//  char       Character
+//  double      Double
+//  int         Integer
+
+// autoboxing = the automatic conversion that the Java compiler makes between the primitive types and their coressponding object wrapper classes
+// unboxing = the reverse of Automatic conversion of wrapper class to primitive
+
+
+
+
+
+public class WrapperClass {
+
+    public static void main(String[] args) {
+        /*
+    Boolean a = true ;
+
+    if(a==true)
+        System.out.println(" This is true ");
+    else{
+            System.out.println(" Its false");
+        }
+    }
+         */
+
+ /*
+    Character b = '@' ;
+
+    if(b=='@')
+        System.out.println(" This is true ");
+    else{
+            System.out.println(" Its false");
+        }
+    }
+         */
+
+ /*
+ Intgear c = 5 ;
+
+    if(c==5)
+        System.out.println(" This is true ");
+    else{
+            System.out.println(" Its false");
+        }
+    }
+         */
+ /*
+       Double d = 5.0;
+
+        if (d == 5) {
+            System.out.println(" This is true "); 
+        }else {
+            System.out.println(" Its false");
+        }
+    } */
+
+    String e = "DEV";
+
+    if ("DEV".equals(e))
+        System.out.println("It's correct");
+        else{
+            System.out.println("It's not Correct");
+        }
+    }
+
+    }
+
+
