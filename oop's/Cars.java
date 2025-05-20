463d1f2ef1241edd0dad704a51606bf38f22c229
DEVAT <atharvfordevelopment@gmail.com>
2025-05-20 14:19:52 +0530

    object's n java

---
+++ oop's/Cars.java
@@ -0,0 +1,17 @@
+
+public class Cars {
+        
+    String name = "Tesla";
+    String model = "Y";
+    int year = 2020;
+    String color ="white";
+    double price =5500.0;
+
+    void drive(){
+        System.out.println("You drive the car");
+    }
+
+    void brake(){
+        System.out.println("You stepped on brakes");
+    }
+}
---
+++ oop's/main.java
@@ -0,0 +1,17 @@
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Cars myCars = new Cars();
+
+        System.out.println(myCars.name);
+        System.out.println(myCars.model);
+        System.out.println(myCars.year);
+        System.out.println(myCars.color);
+        System.out.println(myCars.price);
+
+        myCars.drive();
+        myCars.brake();
+    }
+}
