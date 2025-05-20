e54ac8da82fd1095afba0f0dbe039a7e1c18fee6
DEVAT <atharvfordevelopment@gmail.com>
2025-05-20 19:22:14 +0530

    Constructor overload

---
+++ oop's/tostring/Allcars.java
@@ -0,0 +1,13 @@
+public class Allcars {
+   String maker = "Ford";
+   String model = "Mustang";
+   String color = "red";
+   int year = 2023;
+
+   public Allcars() {
+   }
+
+   public String toString() {
+      return this.maker + "\n" + this.model + "\n" + this.color + "\n" + this.year;
+   }
+}
---
+++ oop's/tostring/ToString.java
@@ -0,0 +1,6 @@
+public class ToString {
+   public static void main(String[] args) {
+      Allcars car = new Allcars();
+      System.out.println(car.toString());
+   }
+}
