61ca4abb3678b38838eb61ba04bc30c22f4eeb58
DEVAT <atharvfordevelopment@gmail.com>
2025-06-03 12:57:56 +0530

    Superkeyword in java

---
+++ oops/SuperKeyWordInJava/SuperKeyword.java
@@ -0,0 +1,10 @@
+public class SuperKeyword{
+
+    public static void main(String[] args) {
+        
+        Hero hero1 = new Hero("Spiderman", 18, "SpiderPower");
+        System.out.println(hero1.name);
+        System.out.println(hero1.age);
+        System.out.println(hero1.power);
+    }
+}
